648d19a59199696c1e6a02f097788ec78bce1990
Khai Tran <khait@firestak.com>
2019-04-12 17:54:18 +0700

    - Integrated Crashlytics. - Config CI

--- app/src/main/java/com/szg_tech/hearthfailure/rest/api/RestClient.java
+++ app/src/main/java/com/szg_tech/hearthfailure/rest/api/RestClient.java
@@ -1,11 +1,9 @@
 package com.szg_tech.hearthfailure.rest.api;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
@@ -14,8 +12,7 @@ import retrofit2.converter.scalars.ScalarsConverterFactory;
  * Created by ahmetkucuk on 3/16/17.
  */
 
-public class RestClient
-{
+public class RestClient {
     //private static final String BASE_URL = "http://api.calchfrisk.net/";
     private static final String BASE_URL = "http://198.71.134.8/api/";
 
@@ -23,26 +20,25 @@ public class RestClient
     private static ApiService apiService;
 
     public RestClient(String token) {
-        Gson gson = new GsonBuilder()
-                .create();
-
         // Add the interceptor to OkHttpClient
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
         OkHttpClient.Builder builder = new OkHttpClient.Builder();
         builder.interceptors().add(new AuthenticationInterceptor(token));
         builder.connectTimeout(120, TimeUnit.SECONDS)
                 .writeTimeout(120, TimeUnit.SECONDS)
-                .readTimeout(120, TimeUnit.SECONDS);
+                .readTimeout(120, TimeUnit.SECONDS)
+                .addInterceptor(logging);
         OkHttpClient client = builder.build();
 
         Retrofit restAdapter = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(ScalarsConverterFactory.create())
-                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addConverterFactory(GsonConverterFactory.create())
                 .client(client)
                 .build();
 
         apiService = restAdapter.create(ApiService.class);
-
     }
 
     public ApiService getApi() {
--- app/src/main/java/com/szg_tech/hearthfailure/rest/authentication/AuthenticationClient.java
+++ app/src/main/java/com/szg_tech/hearthfailure/rest/authentication/AuthenticationClient.java
@@ -1,11 +1,9 @@
 package com.szg_tech.hearthfailure.rest.authentication;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
@@ -18,27 +16,25 @@ public class AuthenticationClient {
 
     //private static final String BASE_URL = "http://api.calchfrisk.net/";
     private static final String BASE_URL = "http://198.71.134.8/api/";
-//    private static final String BASE_URL = "http://heart.xpsign.com/api/";
+    //    private static final String BASE_URL = "http://heart.xpsign.com/api/";
     //private static final String BASE_URL = "http://heart.xpsign.com/";
     private AuthenticationService authenticationService;
 
     public AuthenticationClient() {
-        Gson gson = new GsonBuilder()
-                .create();
-
-
         // Add the interceptor to OkHttpClient
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
         OkHttpClient.Builder builder = new OkHttpClient.Builder();
         builder.connectTimeout(120, TimeUnit.SECONDS)
                 .writeTimeout(120, TimeUnit.SECONDS)
-                .readTimeout(120, TimeUnit.SECONDS);
+                .readTimeout(120, TimeUnit.SECONDS)
+                .addInterceptor(logging);
         OkHttpClient client = builder.build();
-
 
         Retrofit restAdapter = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(ScalarsConverterFactory.create())
-                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addConverterFactory(GsonConverterFactory.create())
                 .client(client)
                 .build();
 
